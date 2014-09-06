17c7358489824cbfdf7258027c82b6a8915d01ec
Raul Miller <rauldmiller@gmail.com>
2014-09-06 11:33:03 -0400

    use C99 fixed width types
    probably would be better to go through and eliminate UINT2 and UINT4 in favor of uint16_t and uint32_t but this will do for now.

--- global.h
+++ global.h
@@ -14,10 +14,10 @@ The following makes PROTOTYPES default to 0 if it has not already
 typedef unsigned char *POINTER;
 
 /* UINT2 defines a two byte word */
-typedef unsigned short int UINT2;
+typedef uint16_t UINT2;
 
 /* UINT4 defines a four byte word */
-typedef unsigned int UINT4;
+typedef uint32_t UINT4;
 
 /* PROTO_LIST is defined depending on how PROTOTYPES is defined above.
 If using PROTOTYPES, then PROTO_LIST returns the list, otherwise it
